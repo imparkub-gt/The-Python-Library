3210987ae5101a717f7c6cb3160e41dfa4a5b9eb
Thomas Raincourt <71552954+imparkub-gt@users.noreply.github.com>
2020-09-19 15:50:09 +0100

    V1.0

---
+++ E1S.py
@@ -0,0 +1,25 @@
+#Exercício1
+#-----------
+
+
+#Enunciado:
+
+#Escreve um programa que pergunte o nome ao utilizador
+#e de seguida imprima: "Olá, nome!"
+#-----------
+
+
+#Resposta:
+
+#Entrada:
+
+nome = input('Qual é o teu nome? ') #Pôr o nome dentro da variável nome
+
+#Processamento:
+
+cumprimento = 'Olá, ' + nome + '!' #Juntar "Olá" com o nome e "!"
+
+#Saída
+
+print(cumprimento) #Imprimir: "Olá, nome!"
+
---
+++ E2S.py
@@ -0,0 +1,24 @@
+#Exercício2
+#-----------
+
+
+#Enunciado:
+
+#Cria uma pequena calculadora
+#que seja capaz de somar dois
+#números dados pelo utilizador
+#-----------
+
+
+#Resposta:
+
+#Entrada:
+
+n1 = input('Escreve um número: ')
+n2 = input('Escreve mais um número: ')
+#Processamento:
+
+resultado = n1 + n2
+
+#Saída
+
