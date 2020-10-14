af950063388d53e9cf9c44e7e423a38154bd1035
Thomas Raincourt <71552954+imparkub-gt@users.noreply.github.com>
2020-10-14 20:21:58 +0100

    e3
    n antes e depois

---
+++ E3S.py
@@ -0,0 +1,10 @@
+#perguntar
+numInteiro = int(input('Diz um número: '))
+#calcular
+num = numInteiro - 1
+#divulgar
+print(f'O número antes de {numInteiro} é {num}!')
+#calcular
+num = numInteiro + 1
+#divulgar
+print(f'O número depois de {numInteiro} é {num}!')
